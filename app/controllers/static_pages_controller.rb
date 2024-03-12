0c790aa6fd783ebe42d4d9229aa87b6bc31a3031
Matt Marini <mattmarini8765@gmail.com>
2024-03-13 08:25:49 +1100

    update index controller with requests to flickr API

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,15 @@
 class StaticPagesController < ApplicationController
 
   def index
-    
+    stripe = Rails.application.credentials.stripe
+
+    flickr = Flickr.new stripe.values[0], stripe.values[1]
+
+    @pics = flickr.photos.search :user_id => '200229639@N07'
+
+    sizes = flickr.photos.getSizes :photo_id => '53578708387'
+
+    original = sizes.find { |s| s.label == 'Original' }
+    @og = original.width
   end
 end
