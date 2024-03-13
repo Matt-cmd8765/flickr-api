cb5e104ee5c8e1780211b788aec3deb918e2c1ff
Matt Marini <mattmarini8765@gmail.com>
2024-03-13 23:53:04 +1100

    Test run to figure out how to put image on index

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,10 +6,14 @@ class StaticPagesController < ApplicationController
     flickr = Flickr.new stripe.values[0], stripe.values[1]
 
     @pics = flickr.photos.search :user_id => '200229639@N07'
+    puts '$$$$$$$$$$$$$$$$$'
+    p @pics
+    puts '$$$$$$$$$$$$$$$$$'
+    server_id = @pics[0].server
+    id = @pics[0].id
+    secret = @pics[0].secret
 
-    sizes = flickr.photos.getSizes :photo_id => '53578708387'
+    @img = "https://live.staticflickr.com/#{server_id}/#{id}_#{secret}_w.jpg"
 
-    original = sizes.find { |s| s.label == 'Original' }
-    @og = original.width
   end
 end
