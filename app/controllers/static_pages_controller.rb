11bcb6fd7f0d6d418258c0c539864c08cd884004
Matt Marini <mattmarini8765@gmail.com>
2024-03-15 22:43:43 +1100

    Flickr user search function working

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,15 +5,14 @@ class StaticPagesController < ApplicationController
 
     flickr = Flickr.new stripe.values[0], stripe.values[1]
 
-    @pics = flickr.photos.search :user_id => '200229639@N07'
-    puts '$$$$$$$$$$$$$$$$$'
-    p @pics
-    puts '$$$$$$$$$$$$$$$$$'
-    server_id = @pics[0].server
-    id = @pics[0].id
-    secret = @pics[0].secret
+    if search_submitted
+      @search = flickr.photos.search :user_id => params[:query].to_s
+    end
+  end
 
-    @img = "https://live.staticflickr.com/#{server_id}/#{id}_#{secret}_w.jpg"
+  private
 
+  def search_submitted
+    params[:query]
   end
 end
